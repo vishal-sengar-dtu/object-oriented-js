ae40cb58694ce49525ea1ea8a40f96f9b0ccea3e
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-02 11:56:35 +0530

    Class Methods

--- object.js
+++ object.js
@@ -3,15 +3,16 @@ class User {
     this.email = email;
     this.name = name;
   }
+  login() {
+    console.log(this.email, 'just logged in');
+  }
+  logout() {
+    console.log(this.email, 'just logged out');
+  }
 }
 
 let userOne = new User('ryu@ninja.com', 'Ryu');
 let userTwo = new User('yoshi@gmail.com', 'Yoshi');
 
-console.log(userOne);
-console.log(userTwo);
-
-// the 'new' keyword
-// - creates a new empty object {}
-// - sets the value of 'this' to be the new empty object
-// - calls the constructor method
+userOne.login();
+userTwo.logout();
