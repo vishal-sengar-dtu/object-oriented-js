ab7ff3f253e64b656203f26b0c18db67536ee030
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-02 14:13:58 +0530

    Constructor function

--- object.js
+++ object.js
@@ -1,37 +1,14 @@
-class User {
-  constructor(email,name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0;
+function User(email, name){
+  this.email = email;
+  this.name = name;
+  this.online = false;
+  this.login = function () {
+    console.log(this.email, 'has logged in');
   }
-  login() {
-    console.log(this.email, 'just logged in');
-    return this;
-  }
-  logout() {
-    console.log(this.email, 'just logged out');
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, 'score is now', this.score);
-    return this;
+  this.logout = function () {
+    console.log(this.email, 'has logged out');
   }
 }
 
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter(u => {
-      return u.email != user.email;
-    })
-  }
-} 
-
 let userOne = new User('ryu@ninja.com', 'Ryu');
 let userTwo = new User('yoshi@gmail.com', 'Yoshi');
-let admin = new Admin('shaun@ninjas.com', 'Shaun');
-
-let users = [userOne, userTwo, admin];
-
-admin.deleteUser(userOne);
-console.log(users);
