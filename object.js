7dc1672e743d726e29a55531c54c179833019849
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-02 12:24:46 +0530

    Method Chaining

--- object.js
+++ object.js
@@ -2,17 +2,25 @@ class User {
   constructor(email,name) {
     this.email = email;
     this.name = name;
+    this.score = 0;
   }
   login() {
     console.log(this.email, 'just logged in');
+    return this;
   }
   logout() {
     console.log(this.email, 'just logged out');
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, 'score is now', this.score);
+    return this;
   }
 }
 
 let userOne = new User('ryu@ninja.com', 'Ryu');
 let userTwo = new User('yoshi@gmail.com', 'Yoshi');
 
-userOne.login();
-userTwo.logout();
+userTwo.login().updateScore().updateScore().logout();
+
