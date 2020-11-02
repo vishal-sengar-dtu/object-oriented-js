5aee8f7cb87aa971e61faabe31dc90cb26197deb
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-02 14:23:33 +0530

    Prototype

--- object.js
+++ object.js
@@ -2,13 +2,20 @@ function User(email, name){
   this.email = email;
   this.name = name;
   this.online = false;
-  this.login = function () {
-    console.log(this.email, 'has logged in');
-  }
-  this.logout = function () {
-    console.log(this.email, 'has logged out');
-  }
 }
+
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, 'has logged in');
+}
+
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, 'has logged out');
+}
+
 
 let userOne = new User('ryu@ninja.com', 'Ryu');
 let userTwo = new User('yoshi@gmail.com', 'Yoshi');
+
+console.log(userOne, userTwo);
